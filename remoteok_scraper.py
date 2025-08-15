525ee783700438522d890d7ee6440f0967452025
Aaryamanlosalka <aaryamanlosalkabtech24@rvu.edu.in>
2025-08-15 12:39:00 +0530

    0004_aaryamanLosalka_G_DS_LAB_1_remoteok_scraper.py

--- remoteok_scraper.py
+++ remoteok_scraper.py
@@ -1,26 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug 15 12:17:20 2025
-
-@author: AARYAMAN
-"""
 
 import requests
 import pandas as pd
-
-# RemoteOK API endpoint
 url = "https://remoteok.com/api"
 
-# Send GET request to API
 response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
 
 if response.status_code == 200:
     data = response.json()
 
-    # Skip metadata in first element
     jobs = data[1:]
 
-    # Prepare structured data
     job_list = []
     for job in jobs:
         company = job.get("company", "").strip()
@@ -35,13 +24,12 @@ if response.status_code == 200:
             "Tags/Features": tags
         })
 
-    # Convert to DataFrame
     df = pd.DataFrame(job_list)
 
-    # Save in clean CSV format
     df.to_csv("remoteok_jobs.csv", index=False, encoding="utf-8")
-    print("✅ RemoteOK jobs saved in 'remoteok_jobs.csv' with clean columns!")
+    print(" RemoteOK jobs saved in 'remoteok_jobs.csv' with clean columns!")
 
 else:
-    print(f"❌ Failed to fetch data. Status code: {response.status_code}")
+    print(f"Failed to fetch data. Status code: {response.status_code}")
+
 
